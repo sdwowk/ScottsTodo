49df7d1685b62d12eb5b6ba24a9b4a785b592bc9
sdwowk <sdwowk@ualberta.ca>
2014-09-18 16:10:39 -0600

    Added activity framework and xml layout for adding to do item

--- gen/com/example/scottstodolist/R.java
+++ gen/com/example/scottstodolist/R.java
@@ -25,21 +25,39 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080001;
-        public static final int listOfToDoView=0x7f080000;
+        public static final int AddDateText=0x7f080003;
+        public static final int AddDateTextView=0x7f080002;
+        public static final int AddTitleText=0x7f080001;
+        public static final int AddTitleTextView=0x7f080000;
+        public static final int AddToDoItemButton=0x7f080006;
+        public static final int NotesText=0x7f080005;
+        public static final int NotesTextView=0x7f080004;
+        public static final int action_settings=0x7f080008;
+        public static final int listOfToDoView=0x7f080007;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_add_item=0x7f030000;
+        public static final int activity_main=0x7f030001;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int add_item=0x7f070000;
+        public static final int main=0x7f070001;
     }
     public static final class string {
         public static final int action_settings=0x7f050002;
+        public static final int add=0x7f05000a;
+        public static final int add_title_here=0x7f050006;
         public static final int app_name=0x7f050000;
+        public static final int date_=0x7f050007;
         public static final int hello_world=0x7f050001;
+        public static final int m_d_yyyy=0x7f050008;
+        public static final int notes_=0x7f050009;
+        public static final int title_=0x7f050004;
+        public static final int title_=0x7f050005;
+        public static final int title_activity_add_item=0x7f050003;
     }
     public static final class style {
+        public static final int AddTitleHint=0x7f060002;
         /** 
         Base application theme, dependent on API level. This theme is replaced
         by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
--- src/com/example/scottstodolist/ToDoItem.java
+++ src/com/example/scottstodolist/ToDoItem.java
@@ -1,6 +1,37 @@
 package com.example.scottstodolist;
 
 public class ToDoItem {
-	private String title;
-	private String dueDate;
+	
+	private String title = "";
+	private String dueDate = "";
+
+	public ToDoItem(String title, String dueDate) {
+		super();
+		this.title = title;
+		this.dueDate = dueDate;
+	}
+	
+	public ToDoItem(String title){
+		this.title = title;
+	}
+	public ToDoItem(){
+		
+	}
+
+	public String getTitle() {
+		return title;
+	}
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	public String getDueDate() {
+		return dueDate;
+	}
+	
+	public void setDueDate(String dueDate) {
+		this.dueDate = dueDate;
+	}
+	
 }
